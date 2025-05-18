2bc50fdd6a8300ecc5b84175605d2037c04a91d9
Giuseppe Pappalardo <06.pappalardo.giuseppe@gmail.com>
2025-05-18 19:36:14 +0200

    Dockerized rust wss, proxied in nginx.conf

--- wss/src/handler.rs
+++ wss/src/handler.rs
@@ -1,7 +1,9 @@
+use crate::*;
 use warp::http::StatusCode;
 
 type WebResult<T> = std::result::Result<T, warp::Rejection>;
 
 pub fn health_handler() -> impl Future<Output = WebResult<impl warp::Reply>> {
+    info!("Received health ping");
     futures::future::ready(Ok(StatusCode::OK))
 }
--- wss/src/main.rs
+++ wss/src/main.rs
@@ -1,14 +1,21 @@
 mod handler;
+mod macros;
 
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    info!("Application started");
+    
     let health = warp::path!("health")
         .and_then(handler::health_handler);
+
+    info!("Created health route");
 
     let routes = health
         .with(warp::cors().allow_any_origin());
 
-    warp::serve(routes).run(([127, 0, 0, 1], 16384)).await;
+    info!("Created base route");
+
+    warp::serve(routes).run(([0, 0, 0, 0], 8888)).await;
 }
